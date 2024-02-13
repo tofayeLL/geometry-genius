dc76511ee910c32b380c1423ad623ddc4771c8e0
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-13 19:55:39 +0600

    added nav bar and finished all the section

--- script/ellipse-area.js
+++ script/ellipse-area.js
@@ -8,4 +8,7 @@ function calculateEllipseArea(){
     const eArea = 3.14 * major * minor;
     
     setInnnerTextById('ellipse-area', eArea);
+
+    // to display area in area calculator
+    // setInnnerTextById('ellipse-area1', eArea);
 }
